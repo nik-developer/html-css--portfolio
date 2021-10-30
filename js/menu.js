b64a945850d7f994c9db70fb2bfe3ae05898dbb4
Nikita Bielykh <nikita.bielykh@gmail.com>
2021-10-30 18:21:07 +0300

    last version

--- js/menu.js
+++ js/menu.js
@@ -1,34 +1,62 @@
 // modal/open/close
 
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
 
 // mobile-menu
 const menuBtn = document.querySelector('.menu-btn-js');
 const mobileMenuRef = document.querySelector('.menu-container-js');
 menuBtn.addEventListener('click', toggleMenuHandler);
 function toggleMenuHandler() {
-  menuBtn.classList.toggle('is-open');
-  const isOpenMenu = menuBtn.classList.contains('is-open');
-  if (isOpenMenu) {
-    mobileMenuRef.style.display = 'block';
+    menuBtn.classList.toggle('is-open');
+    const isOpenMenu = menuBtn.classList.contains('is-open');
+    console.log(isOpenMenu);
+    if (isOpenMenu) {
+        mobileMenuRef.style.display = "block";
+        return
+    }
+    if (!isOpenMenu) {
+        mobileMenuRef.style.display = "none";
+    }
+}
+
+window.addEventListener('resize', getTabletMenuSize);
+function getTabletMenuSize(event){
+  console.log(event.target);
+  const currentScreenWidth = event.target.screen.width;
+  if(currentScreenWidth >= 768){
+    mobileMenuRef.style.display = "flex";
+
     return;
   }
-  if (!isOpenMenu) {
-    mobileMenuRef.style.display = 'none';
+  if(currentScreenWidth < 768){
+    console.log('here');
+    mobileMenuRef.style.display = "block";
+    mobileMenuRef.style.display = "none";
+    const isOpenMenu = menuBtn.classList.contains('is-open');
+    if(isOpenMenu) {
+      console.log('here');
+      menuBtn.classList.remove('is-open');
+    }
+    return;
   }
 }
+
+
 
 // (() => {
 //   const refs = {
@@ -44,13 +72,13 @@ function toggleMenuHandler() {
 // })();
 
 // placeholder
-// document.querySelectorAll('.form-input').forEach(element => {
-//   element.addEventListener('focusin', e => {
-//     e.currentTarget.placeholder = e.currentTarget.dataset.placeholder;
-//   });
-// });
-// document.querySelectorAll('.form-input').forEach(element => {
-//   element.addEventListener('focusout', e => {
-//     e.currentTarget.placeholder = ' ';
-//   });
-// });
+document.querySelectorAll('.form-input').forEach(element => {
+  element.addEventListener('focusin', e => {
+    e.currentTarget.placeholder = e.currentTarget.dataset.placeholder;
+  });
+});
+document.querySelectorAll('.form-input').forEach(element => {
+  element.addEventListener('focusout', e => {
+    e.currentTarget.placeholder = '';
+  });
+});
